194557c6ac7e829f753695b670ec9ea424dad5d9
maze.chang <maze.chang@afusiom.com>
2019-04-09 10:42:58 +0800

    test

--- services/TestService.go
+++ services/TestService.go
@@ -10,3 +10,10 @@ type TestService struct {
 func (s *TestService) MyTest(to string) string {
 	return "hi " + to
 }
+
+type Test2Service struct {
+}
+
+func (s *Test2Service) MyTest(to string) string {
+	return "hiiii " + to
+}
